ec88f86418235eb6fb7d8b9905b43ccae7e8a90b
chanelp <mariannissanchez27@gmail.com>
2023-11-20 14:00:37 -0400

    Uso de ngFor: agregar y eliminar task

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,10 +9,22 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
   tasks = signal([
     "Learn English",
     "Read a book",
     "Help with homeworks",
     "Programming"
   ]);
+
+  changeHandler(event: Event){
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  deleteTask(index: number){
+    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+  }
+
 }
